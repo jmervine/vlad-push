c2d4a11e9137390376a04ba4a7a08a16ab9f0ce9
Joshua Mervine <joshua@mervine.net>
2011-07-20 21:15:18 -0700

    major rewrite, removing all scm requirements -- not yet functional

--- lib/vlad/push.rb
+++ lib/vlad/push.rb
@@ -1,72 +1,91 @@
 require 'vlad'
+class Vlad::Push
+  VERSION = "0.1.0"
 
-module Vlad
-  module Push
-    VERSION = '0.0.2' #:nodoc:
-  end
-end
+  set :source, Vlad::Push.new
 
-namespace :vlad do
-
+  # default to be overwritten by deploy.rb as needed
   set :repository,      "/tmp/repo"
-  set :scm,             :git
-  set :push_scp,        "scp"
+  set :scm,             :push
+  set :push_scp, "scp"
+  set :ssh_flags, ""
 
-  def export_cmd
-    if scm.to_s == "git"
-      scm_src = ".git"
-    else
-      raise "#{scm.to_s} not yet supported."
+  # telling vlad not to bother running checkout
+  #  but this doesn't seem to work, I'll probably
+  #  moving it to the Rakefile
+  #set :skip_scm, true 
+
+  def checkout(revision, destination)
+    "echo 'skipping checkout per vlad-push'"
+  end
+
+  def export(source, destination)
+    # ignoring source
+    [ "mkdir -p #{repository}",
+      "cd #{repository}",
+      "tar -xzf /tmp/#{application}-#{release_name}.tgz",
+      "cp -r #{repository} #{destination}"
+    ].join(" && ")
+  end
+
+  def revision(revision)
+  # telling vlad to use "repository" as "revision" as we have no scm
+    repository
+  end
+
+  def push(host, destination)
+    [ "#{push_scp} #{ssh_flags}",
+      "/tmp/#{application}-#{release_name}.tgz",
+      #"#{host}:#{destination}/#{application}-#{release_name}.tgz"
+      "#{host}:/tmp/#{application}-#{release_name}.tgz"
+    ].join(" ")
+  end
+
+  def push_extract
+    [ "cd #{repository}",
+      "tar -xzf #{application}-#{release_name}.tgz"
+    ].join(" && ")
+  end
+
+  def push_cleanup
+    [ "rm -vrf /tmp/#{application}-*.tgz",
+      [ "if [ -e #{repository} ]",
+        "then rm -rf #{repository}",
+        "fi"
+      ].join("; ")
+    ].join(" && ")
+  end
+
+  def compress
+    [ "tar -czf /tmp/#{application}-#{release_name}.tgz",
+      "--exclude \"\.git*\"",
+      "--exclude \"\.svn*\"",
+      "."
+    ].join(" ") 
+  end
+
+  namespace :vlad do
+
+    desc "Push current working directory to remote servers."
+    task :push do
+      sh source.compress
+      domain.each do |host|
+        sh source.push(host, repository)
+      end
+      #run source.push_extract
     end
-    cmd = "tar -czf /tmp/#{application}-#{release_name}.tgz #{scm_src}"
-    cmd
-  end
 
-  def push_cmd(host)
-    cmd =  "#{push_scp} #{ssh_flags.join(" ")} "
-    cmd << "/tmp/#{application}-#{release_name}.tgz "
-    cmd << "#{host}:/tmp/#{application}-#{release_name}.tgz" 
-    cmd
-  end
-
-  def extract_cmd
-    cmd =  "mkdir -p #{repository} && "
-    cmd << "cd #{repository} && "
-    cmd << "tar -xzf /tmp/#{application}-#{release_name}.tgz"
-    cmd
-  end
-
-  def cleanup_cmd
-    cmd = []
-    cmd << %(if [ -e /tmp/#{application}-*.tgz ])
-    cmd << %(then rm -f /tmp/#{application}-*.tgz)
-    cmd << %(fi)
-    cmd.join("; ")
-  end
-
-  desc "Push current working directory as self contained repo on remote server repository."
-  task :push do
-    sh export_cmd
-    domain.each do |host|
-      sh push_cmd(host)
+    # Updating 'vlad:cleanup' to include post-task
+    remote_task :cleanup do
+      Rake::Task['vlad:push_cleanup'].invoke
     end
-    Rake::Task['vlad:after_push'].invoke
+
+    desc "Clean up archive files created by push. This will also be run by vlad:cleanup."
+    remote_task :push_cleanup do
+      sh source.push_cleanup
+      run source.push_cleanup
+    end
+
   end
-
-  remote_task :after_push do
-    puts "Extracting /tmp/#{application}-#{release_name}.tgz to #{repository}"
-    run extract_cmd
-  end
-
-  # Updating 'vlad:cleanup' to include post-task
-  remote_task :cleanup do
-    Rake::Task['vlad:after_cleanup'].invoke
-  end
-
-  remote_task :after_cleanup do
-    sh cleanup_cmd
-    run cleanup_cmd
-  end
-
-end
-
+  
+end # class def
