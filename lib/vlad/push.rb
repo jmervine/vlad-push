c0b21f5b9857ec543cc32ba5d080138b12671ac5
nathan r. hruby <nhruby@gmail.com>
2012-01-18 13:41:58 -0700

    change version to make older rubygem happy (eg: centos/rhel5) and add entry in History.txt too

--- lib/vlad/push.rb
+++ lib/vlad/push.rb
@@ -4,7 +4,7 @@ require 'vlad'
 #
 # Please see {file:lib/vlad/push.rb Vlad::Push Source} for Rake task documentation.
 class Vlad::Push
-  VERSION = "1.1b1"
+  VERSION = "1.1.0"
 
   # @attribute[rw]
   # init Vlad::Push
